62b734faaf3b7734238393b12160f606599768cc
Hairum Qureshi <hairum@mac.myfiosgateway.com>
2026-09-01 17:22:51 -0400

    added stylish icons to side nav

--- apps/frontend/src/components/SideNav.tsx
+++ apps/frontend/src/components/SideNav.tsx
@@ -1,3 +1,21 @@
+import { LuPanelLeft } from "react-icons/lu";
+import { IoChatbox } from "react-icons/io5";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 export default function SideNav() {
-	return <div className="w-15 p-3 bg-blue-600 min-h-screen"></div>;
+	return (
+		<div className="w-15 p-3 bg-blue-600 min-h-screen">
+			<div className="flex flex-col items-center space-y-6">
+				<div className="flex flex-col items-center space-y-2 hover:cursor-pointer">
+					<LuPanelLeft className="text-white text-lg" />
+				</div>
+				<div className="flex flex-col items-center space-y-2 hover:cursor-pointer">
+					<FaMagnifyingGlass className="text-white text-lg" />
+				</div>
+				<div className="flex flex-col items-center space-y-2 bg-blue-800 p-2 rounded-lg hover:cursor-pointer">
+					<IoChatbox className="text-white text-lg" />
+				</div>
+			</div>
+		</div>
+	);
 }
