30dbe10f4bb7607fd56c510795143d7bca6f8fc7
Hairum Qureshi <hairum@wifi-roaming-128-4-83-220.wifi.udel.edu>
2026-09-01 13:58:03 -0400

    deleted About and NotFound components

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,3 +1,11 @@
+import SideNav from "../components/SideNav";
+import ChatContainer from "../components/ChatContainer";
+
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="flex">
+			<SideNav />
+			<ChatContainer />
+		</div>
+	);
 }
