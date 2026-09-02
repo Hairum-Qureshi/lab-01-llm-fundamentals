823545b6ef9475690be202eb256da0cba2228170
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-02 19:47:58 -0400

    changed ask endpoint so it's a GET request and takes in a query param instead of a body

--- apps/backend/src/llm/llm.controller.ts
+++ apps/backend/src/llm/llm.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Sse } from '@nestjs/common';
+import { LlmService } from './llm.service';
 
 @Controller('llm')
-export class LlmController {}
+export class LlmController {
+  constructor(private readonly llmService: LlmService) {}
+
+  @Get('ask')
+  @Sse()
+  async askQuestion(@Query('query') query: string) {
+    return this.llmService.generateResponse(query);
+  }
+}
