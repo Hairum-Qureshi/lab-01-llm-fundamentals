c0f7d67135852cca5592a5cb7b892b7a31fb8186
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-02 12:22:17 -0400

    added user icon at the bottom of side nav

--- apps/frontend/src/components/SideNav.tsx
+++ apps/frontend/src/components/SideNav.tsx
@@ -1,6 +1,7 @@
 import { LuPanelLeft } from "react-icons/lu";
 import { IoChatbox } from "react-icons/io5";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaCircleUser } from "react-icons/fa6";
 
 export default function SideNav() {
 	return (
@@ -14,6 +15,11 @@ export default function SideNav() {
 				</div>
 				<div className="flex flex-col items-center space-y-2 bg-blue-800 p-2 rounded-lg hover:cursor-pointer">
 					<IoChatbox className="text-white text-lg" />
+				</div>
+				<div className="flex flex-col items-center space-y-2 hover:cursor-pointer">
+					<div className="flex flex-col items-center space-y-2 hover:cursor-pointer absolute bottom-0 mb-5">
+						<FaCircleUser className="text-white text-3xl" />
+					</div>
 				</div>
 			</div>
 		</div>
