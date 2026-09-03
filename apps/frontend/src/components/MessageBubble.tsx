6c8153227d46f93ca5d7010b5b56efbf8508ddd5
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-02 21:12:10 -0400

    added Tailwind Typography classes to render markdown syntax styling

--- apps/frontend/src/components/MessageBubble.tsx
+++ apps/frontend/src/components/MessageBubble.tsx
@@ -1,7 +1,43 @@
-export default function MessageBubble() {
+import Markdown from "react-markdown";
+
+export default function MessageBubble({
+	message,
+	isAIResponse
+}: {
+	message: string;
+	isAIResponse: boolean;
+}) {
 	return (
-		<div className="bg-blue-600 text-white p-3 w-3/4 rounded-md flex ml-auto">
-			MessageBubble
+		<div
+			className={`p-3 w-3/4 rounded-md h-auto wrap-break-word ${
+				isAIResponse
+					? `
+            [&_h1]:text-2xl
+            [&_h1]:font-bold
+            [&_h1]:mb-4
+            [&_h3]:text-xl
+            [&_h3]:font-bold
+            [&_h3]:mb-4
+            [&_p]:mb-2
+            [&_blockquote]:pl-4
+            [&_blockquote]:border-l-4
+            [&_blockquote]:border-sky-500
+            [&_blockquote]:italic
+            [&_blockquote]:text-slate-200
+            [&_ul]:list-disc
+            [&_ul]:pl-6
+            [&_ol]:list-decimal
+            [&_ol]:pl-6
+            [&_li]:mb-1
+          `
+					: ""
+			} ${
+				isAIResponse
+					? "bg-blue-600 text-white mr-auto"
+					: "bg-gray-300 text-black ml-auto"
+			}`}
+		>
+			{isAIResponse ? <Markdown>{message}</Markdown> : message}
 		</div>
 	);
 }
