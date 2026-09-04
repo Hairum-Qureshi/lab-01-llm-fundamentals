8b64c0ec4c0dbc269f0090c0e44892bb0b905591
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-04 12:15:25 -0400

    removed App service constructor and added simple GET endpoint that returns server status

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get()
+  serverStatus() {
+    return {
+      status: 'Server is running',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
